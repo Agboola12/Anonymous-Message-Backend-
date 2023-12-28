fd04920ccec8668e17b4f170aa79dd1640f32286
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-28 23:37:28 +0100

    working on the functionality

--- controller/messageController.js
+++ controller/messageController.js
@@ -3,7 +3,6 @@ const userMessage = require("../model/messageModel");
 
 // Registering User
 const message = async (req, res) => {
-    console.log(req.body);
     const {userId, message  } = req.body;
     try {
         const newUser = await userMessage.create({ userId,message });
@@ -26,21 +25,20 @@ const getMessage = (req, res)=>{
     const id = req.params.id;
     userMessage.find({userId : id})
         .then(data => {
-            if (data) {
-                console.log(data);
+            if (data.length > 0) {
+                // console.log(data);
                 res.status(200).send({
-                    success: true,
+                    status: true,
                     message: "successful in getting users message",
                     data
                 })
             }
         }).catch(err => {
             res.status(500).send({
-                success: false,
-                message: "not successful in getting users message",
+                status: false,
+                message: "No messages found for the user",
             })
-
-            
+            console.log("Error in getting user's message:", err);
         })
 
 }
