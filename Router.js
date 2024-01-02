49370206824e0476a17d24cc5369f059ca931f21
Agbooa12 <agboolaelijah077@gmail.com>
2024-01-02 20:56:53 +0100

    working on the functionality

--- Router.js
+++ Router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getUser } = require('./controller/userController');
+const { register, login, getUser, getUserMessage } = require('./controller/userController');
 const { message, getMessage } = require('./controller/messageController');
 const { verifyUser } = require('./middleware/authMiddleware');
 const rout = express.Router();
@@ -8,7 +8,7 @@ const rout = express.Router();
 rout.post("/register", register )
 rout.post("/login", login )
 rout.get("/getUser",verifyUser, getUser)
-rout.get("/getUser", getUser)
+rout.get("/getUserMessage/:id", getUserMessage)
 
 // message 
 rout.post("/message", message )
