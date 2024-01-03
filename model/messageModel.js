273c2f933db256f12f112e3569a80df936398568
Agbooa12 <agboolaelijah077@gmail.com>
2024-01-03 18:34:34 +0100

    working on the functionality

--- model/messageModel.js
+++ model/messageModel.js
@@ -3,7 +3,7 @@ const  mongoose  = require('mongoose');
 const userMessageSchema = mongoose.Schema({
     userId: String,
     message: String,
-})
+}, {timestamps:true})
 
 const userMessage = mongoose.model("userMessage", userMessageSchema);
 
